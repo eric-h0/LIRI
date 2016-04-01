f1d2d365b4b75068c87046d60c442665f40990f5
E-Hizzle <eho.rutgers@gmail.com>
2016-04-01 12:01:56 -0400

    Added basic logic for switches. Look over this again later. Nodemon is perfect for this homework btw.

--- liri.js
+++ liri.js
@@ -1,5 +1,21 @@
 var keys = require('./keys.js')
 
+//Something like this.
+switch (process.argv[2]) {
+  case "my-tweets";
+    twitter();
+    break;
+  case "spotify-this-song";
+    spotify();
+    break;
+  case "movie-this";
+    omdb();
+  case "do-what-it-says";
+    whatItSays();
+  default:
+    console.log("I do not understand you, human.")
+}
+
 // Make it so liri.js can take in one of the following arguments
 
 // my-tweets
