84c7919cb314a6e229e108fe520bcd5e8f1d06a7
E-Hizzle <eho.rutgers@gmail.com>
2016-04-02 09:45:34 -0400

    Figuring out node.

--- liri.js
+++ liri.js
@@ -1,20 +1,20 @@
 var keys = require('./keys.js')
 
 //Something like this.
-switch (process.argv[2]) {
-  case "my-tweets";
-    twitter();
-    break;
-  case "spotify-this-song";
-    spotify();
-    break;
-  case "movie-this";
-    omdb();
-  case "do-what-it-says";
-    whatItSays();
-  default:
-    console.log("I do not understand you, human.")
-}
+// switch (process.argv[2]) {
+//   case "my-tweets";
+//     twitter();
+//     break;
+//   case "spotify-this-song";
+//     spotify();
+//     break;
+//   case "movie-this";
+//     omdb();
+//   case "do-what-it-says";
+//     whatItSays();
+//   default:
+//     console.log("I do not understand you, human.")
+// }
 
 // Make it so liri.js can take in one of the following arguments
 
