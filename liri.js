6a70716d7ac887c3a6326a09f361bbba4f0eefcf
E-Hizzle <eho.rutgers@gmail.com>
2016-04-05 15:47:34 -0400

    Cleaned up movies.

--- liri.js
+++ liri.js
@@ -1,33 +1,36 @@
+var fs=require('fs');
 var keys = require('./keys.js');
 var request = require ("request");
 var type = process.argv[2];
+var Twitter = require('twitter');
+var spotify = require('spotify');
 
-if(type == "my-tweets"){
+if(type == "tweets"){
   console.log("Twitter")
-}
+};
 
-if(type == "spotify-this-song"){
+if(type == "spotify"){
   console.log("Spotify")
-}
+};
 
-if(type == "movie-this"){
+if(type == "movie"){
   var movie = process.argv[3];
   request('http://www.omdbapi.com/?t='+movie+'&y=&plot=short&tomatoes=true&r=json', function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-       var json = JSON.parse(body);
-       console.log("Title: "+json.Title);
-       console.log("Year: "+json.Year);
-       console.log("IMDB Rating: "+json.imdbRating);
-       console.log("Country: "+json.Country);
-       console.log("Language: "+json.Language);
-       console.log("Plot: "+json.Plot);
-       console.log("Actors: "+json.Actors);
-       console.log("Rotten Tomatoes rating: "+json.tomatoRating);
-       console.log("Rotten Tomatoes URL: "+json.tomatoURL);
-       }
-    })
+    if (!error && response.statusCode == 200){
+         var json = JSON.parse(body);
+         console.log("\nTitle: "+json.Title);
+         console.log("Year: "+json.Year);
+         console.log("IMDB Rating: "+json.imdbRating);
+         console.log("Country: "+json.Country);
+         console.log("Language: "+json.Language);
+         console.log("Plot: "+json.Plot);
+         console.log("Actors: "+json.Actors);
+         console.log("Rotten Tomatoes rating: "+json.tomatoRating);
+         console.log("Rotten Tomatoes URL: "+json.tomatoURL+"\n");
+         };
+    });
 };
 
 if(type == "do-what-it-says"){
   console.log("Do what it says.")
-}
+};
