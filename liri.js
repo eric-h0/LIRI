f4c0d12bde14d46923a3104b6de70522cdcdd903
E-Hizzle <eho.rutgers@gmail.com>
2016-04-06 11:43:14 -0400

    Finished homework

--- liri.js
+++ liri.js
@@ -1,50 +1,71 @@
-var fs      = require('fs');
-var keys    = require('./keys.js');
-var request = require ("request");
+var fs = require('fs');
+var keys = require('./keys.js');
+var request = require("request");
 var Twitter = require('twitter');
 var spotify = require('spotify');
-var colors  = require('colors');
-var type    = process.argv[2];
-var client  = new Twitter({
+var colors = require('colors');
+var type = process.argv[2];
+var client = new Twitter({
   consumer_key: 'GsXabDjw7KZwCvI3O38TVen1d',
   consumer_secret: 'biWBL3MuKq1kFVxtNBv3Y0LhVM23xwnBsDF3Kmabhkz6tZTnwG',
   access_token_key: '166452344-FLd0gUjnJ7p5wTVSiRJAYInaXva5y0VmDoNDmVkX',
   access_token_secret: '8UW9NLgrzQMAPtj5119bp7qvVlOu4k3vGzL5vfPGW7BSJ'
 });
 
-if(type == "tweets"){
-    var params = {screen_name: 'E_Hizzle'};
-    client.get('statuses/user_timeline', params, function(error, tweets, response){
+if (type == "tweets") {
+  var params = { screen_name: 'E_Hizzle' };
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
     if (!error) {
-      for(i=0;i<process.argv[3];i++){
-        console.log("\n@E_Hizzle said: ".red + "\n"+ tweets[i].text.blue + "\n" + "On: " + tweets[i].created_at.green.underline + "." + "\n")
+      for (i = 0; i < 20; i++) {
+        console.log("\n@E_Hizzle said: ".red + "\n" + tweets[i].text.blue + "\n" + "On: " + tweets[i].created_at.green.underline + "." + "\n")
       };
     };
-  });  
+  });
 };
 
-if(type == "spotify"){
-  console.log("Spotify")
-}; 
+if (type == "spotify") {
+  var songTitle = process.argv[3]
+  if (songTitle === undefined) {
+    songTitle = "what's my age again";
+  }
+  spotify.search({ type: 'track', query: songTitle }, function(err, data) {
+    if (err) {
+      console.log('Error occurred: ' + err);
+      return;
+    }
 
-if(type == "movie"){
+    var items = data.tracks.items;
+    for (i = 0; i < items.length; ++i) {
+      console.log("\nSong Name: ".bold.green + items[i].name.rainbow);
+      console.log("Spotify Link: ".bold.green + items[i].preview_url.blue);
+      console.log("Album Name: ".bold.green + items[i].album.name.red);
+      for (k = 0; k < items[i].artists.length; ++k) {
+        console.log("Artist: ".bold.green + items[i].artists[k].name.red);
+      }
+    }
+    console.log("\n");
+
+  });
+};
+
+if (type == "movie") {
   var movie = process.argv[3];
-  request('http://www.omdbapi.com/?t='+movie+'&y=&plot=short&tomatoes=true&r==json', function (error, response, body) {
-    if (!error && response.statusCode == 200){
-         var json = JSON.parse(body);
-         console.log("\nTitle".bold.underline+": "+json.Title.rainbow.underline + ".");
-         console.log("Year".bold.underline+": "+json.Year.green + ".");
-         console.log("IMDB Rating".bold.underline+": "+json.imdbRating.green + ".");
-         console.log("Country".bold.underline+": "+json.Country.green + ".");
-         console.log("Language".bold.underline+": "+json.Language.green + ".");
-         console.log("Plot".bold.underline+": "+json.Plot.green);
-         console.log("Actors".bold.underline+": "+json.Actors.green + ".");
-         console.log("Rotten Tomatoes rating".bold.underline+": "+json.tomatoRating.green + ".");
-         console.log("Rotten Tomatoes URL".bold.underline+": "+json.tomatoURL.green+ "." + "\n");
-         };
-    });
+  request('http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&tomatoes=true&r==json', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var json = JSON.parse(body);
+      console.log("\nTitle".bold.underline + ": " + json.Title.rainbow.underline + ".");
+      console.log("Year".bold.underline + ": " + json.Year.green + ".");
+      console.log("IMDB Rating".bold.underline + ": " + json.imdbRating.green + ".");
+      console.log("Country".bold.underline + ": " + json.Country.green + ".");
+      console.log("Language".bold.underline + ": " + json.Language.green + ".");
+      console.log("Plot".bold.underline + ": " + json.Plot.green);
+      console.log("Actors".bold.underline + ": " + json.Actors.green + ".");
+      console.log("Rotten Tomatoes rating".bold.underline + ": " + json.tomatoRating.green + ".");
+      console.log("Rotten Tomatoes URL".bold.underline + ": " + json.tomatoURL.green + "." + "\n");
+    };
+  });
 };
 
-if(type == "do-what-it-says"){
+if (type == "do-what-it-says") {
   console.log("Do what it says.")
 };
