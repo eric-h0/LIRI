f29a862983006bbb0bb5dcdf2a40ca5bf83f2bb4
E-Hizzle <eho.rutgers@gmail.com>
2016-04-07 16:20:33 -0400

    Removed keys

--- keys.js
+++
@@ -1,6 +0,0 @@
-exports.twitterKeys = {
-  consumer_key: 'GsXabDjw7KZwCvI3O38TVen1d',
-  consumer_secret: 'biWBL3MuKq1kFVxtNBv3Y0LhVM23xwnBsDF3Kmabhkz6tZTnwG',
-  access_token_key: '166452344-FLd0gUjnJ7p5wTVSiRJAYInaXva5y0VmDoNDmVkX',
-  access_token_secret: '8UW9NLgrzQMAPtj5119bp7qvVlOu4k3vGzL5vfPGW7BSJ',
-}
